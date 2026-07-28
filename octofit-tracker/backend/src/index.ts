c5b8bc10c1f4fe26f503970f44045cb0d7c74631
gunarsblumbahs <54351114+gunarsblumbahs@users.noreply.github.com>
2026-07-28 15:56:11 +0000

    7

--- octofit-tracker/backend/src/index.ts
+++ octofit-tracker/backend/src/index.ts
@@ -15,7 +15,8 @@ const port = Number(process.env.PORT || 8000);
 const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/octofit_db';
 
 const getApiBaseUrl = () => {
-  const codespaceName = process.env.CODESPACE_NAME || 'my-demo-gb';
+//  const codespaceName = process.env.CODESPACE_NAME || 'my-demo-gb';
+  const codespaceName = 'my-demo-gb';
   return codespaceName
     ? `https://${codespaceName}-8000.app.github.dev`
     : 'http://localhost:8000';
